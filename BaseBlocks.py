b1b21ae0dd2ede981064edd3bad9f4f1f442cfba
ludaskauro <ludvig.askbom@aurobay.com>
2025-03-27 17:26:16 +0100

    Add files via upload

--- BaseBlocks.py
+++ BaseBlocks.py
@@ -1,5 +1,7 @@
 from BaseClasses import Block
+from scipy.interpolate import RegularGridInterpolator 
 import numpy as np
+import plotly.graph_objects as go
 
 class EntryCondition(Block):
     def __init__(self, name: str, entrySignal) -> None:
@@ -324,6 +326,131 @@ class Output(Block):
         
         self.node = [{'data':{'id':self.name,'label':name}}] + [{'data': {'id': self.name + '_' + out, 'parent': self.name, 'label':'Out'}} for out in inputs]
                     
-    
     def computeOutput(self):
         self.outputs = self.inputs
+
+class Map2D(Block):
+    def __init__(self,name,x,y,output,DCMPath):
+        super().__init__(name, [x,y], [output])
+        
+        self.label = '2D Map ' + name.split('_')[-1]
+
+        self.node = [{'data':{'id':self.name,'label':self.label}},
+                    {'data': {'id': self.name + '_' + x,'label':'x', 'parent': self.name},'style':{'text-valign': 'bottom', 'text-halign': 'center'}},
+                    {'data': {'id': self.name + '_' + y,'label':'y', 'parent': self.name},'style':{'text-valign': 'bottom', 'text-halign': 'center'}},
+                    {'data': {'id': self.name + '_' + output, 'parent': self.name, 'label':'z'}}]
+    
+        with open(DCMPath,'r') as file:
+            map_started = False
+            x,y,z = [],[],[]
+            
+            for line in file.readlines():
+                line = line.strip()
+
+                if line.startswith('WERT'):
+                    tmp = line.split()[1:]
+                    z.append([float(i) for i in tmp])
+                
+                if line.startswith('*SSTX') or line.startswith('*SSTY'):
+                    map_started = True
+                
+                if line.startswith('END') and map_started:
+                    break
+
+                if line.startswith('ST/X'):
+                    tmp = line.split()[1:]
+                    x.extend([float(i) for i in tmp])
+                
+                if line.startswith('ST/Y'):
+                    y.append(float(line.split()[1]))
+        
+        self.xPoint = x
+        self.yPoint = y
+
+        self.grid = RegularGridInterpolator(points=(x,y),values=z,bounds_error=False)
+
+    def computeOutput(self):        
+        self.inputs = {signal:np.clip(value,min(coord),max(coord)) for coord, (signal, value) in zip([self.yPoint,self.xPoint], self.inputs.items())}
+        self.outputs[self.outputs_list[0]] = self.grid((self.inputs[self.inputs_list[1]],self.inputs[self.inputs_list[0]]))
+
+    def visualizeMap(self):
+        x, y = np.meshgrid(self.yPoint, self.xPoint)
+        z = self.grid.values
+
+        lines = []
+        line_marker = dict(color='#000000', width=4)
+        for i, j, k in zip(x, y, z):
+            lines.append(go.Scatter3d(x=i, y=j, z=k, mode='lines', line=line_marker))
+        for i, j, k in zip(x.T, y.T, z.T):
+            lines.append(go.Scatter3d(x=i, y=j, z=k, mode='lines', line=line_marker))
+
+
+        trace2 = go.Surface(z=z, x=x, y=y,showscale=False)
+
+        fig = go.Figure(lines)
+        fig.add_trace(trace2)
+
+        fig.update_layout(
+            title=self.name,
+            scene=dict(
+                xaxis_title=self.inputs_list[0],
+                yaxis_title=self.inputs_list[1],
+                zaxis_title=self.outputs_list[0]
+            ), 
+            showlegend=False
+        )    
+        fig.show()
+
+class Map1D(Block):
+    def __init__(self,name,x,output,DCMPath):
+        super().__init__(name, [x], [output])
+        
+        self.label = '1D Map ' + name.split('_')[-1]
+
+        self.node = [{'data':{'id':self.name,'label':self.label}},
+                    {'data': {'id': self.name + '_' + x,'label':'x', 'parent': self.name},'style':{'text-valign': 'bottom', 'text-halign': 'center'}},
+                    {'data': {'id': self.name + '_' + output, 'parent': self.name, 'label':'y'}}]
+    
+        with open(DCMPath,'r') as file:
+            map_started = False
+            x,y = [],[]
+            
+            for line in file.readlines():
+                line = line.strip()
+
+                if line.startswith('WERT'):
+                    tmp = line.split()[1:]
+                    y.extend([float(i) for i in tmp])
+                
+                if line.startswith('*SSTX') or line.startswith('*SSTY'):
+                    map_started = True
+                
+                if line.startswith('END') and map_started:
+                    break
+
+                if line.startswith('ST/X'):
+                    tmp = line.split()[1:]
+                    x.extend([float(i) for i in tmp])
+                
+        self.xPoint = x
+
+        self.grid = RegularGridInterpolator(points=(x,),values=y,bounds_error=False)
+
+    def computeOutput(self):  
+        #not super pretty      
+        self.inputs = {signal:np.clip(value,min(self.xPoint),max(self.xPoint)) for signal, value in self.inputs.items()}
+        data = self.inputs[self.inputs_list[0]]
+        data_shape = data.shape
+        data = data.flatten()
+        func = lambda x:self.grid([x])
+        self.outputs[self.outputs_list[0]] = np.array(list(map(func,data))).reshape(data_shape)
+
+    def visualizeMap(self):
+        fig = go.Figure(data=go.Scatter(x=self.xPoint, y=self.grid.values))
+        fig.update_layout(
+            title=self.name,
+            xaxis_title=self.inputs_list[0],
+            yaxis_title=self.outputs_list[0],
+            showlegend=False
+        )    
+        fig.show()
